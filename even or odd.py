657cc869653db49f9a6082db541496c0e317b90b
K Harishankar <48408097+Haritronic909@users.noreply.github.com>
2019-09-09 12:25:17 +0530

    Create even or odd.py

---
+++ even or odd.py
@@ -0,0 +1,9 @@
+number = int(input("enter number\n"))
+if ( number >= 0 ):
+   if ( number % 2 == 0 ):
+       print ("even")
+   else:
+       print ("odd")
+else:
+   print ("negative")
+print ("all done")
